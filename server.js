1dcc368613bfbadbfe8b1d0256d270fd05ef6fe8
Kaan <kbkabadayi7@gmail.com>
2021-08-04 19:46:16 +0300

    aaaaaaaaaaaa

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const signin = require("./controllers/signin");
 const profile = require("./controllers/profile");
 const image = require("./controllers/image");
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = knex({
     client: 'pg',
     connection: {
@@ -37,4 +39,6 @@ app.put("/image", (req, res) => { image.handleImage(req, res, db) });
 app.listen(process.env.PORT || 3000, () => {
     console.log(`app is running on ${process.env.PORT}`);
 })
+
+console.log("yarak");
 
