587e8d03ff9c657f33a700e1b690a19116a2479d
Kaan <kbkabadayi7@gmail.com>
2021-08-08 18:55:33 +0300

    şimdi oldu

--- server.js
+++ server.js
@@ -14,8 +14,9 @@ const db = knex({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: true
-
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
 
